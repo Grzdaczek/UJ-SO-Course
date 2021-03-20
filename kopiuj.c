4b6980769e3d4d0d5f8d0e0c824adc3d4d94bccb
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-03-21 00:47:27 +0100

    Zmiana funkcji na systemowe

--- kopiuj.c
+++ kopiuj.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 
 int main(int argc, char* argv[]) {
 
     if(argc != 3) {
-        perror("err: improper usage. $ ./kopiuj.x <src file> <dist file>");
+        printf("err: improper usage. \n$./kopiuj.x <src file> <dist file>\n");
+        exit(1);
+    }
+
+    int src = open(argv[1], O_RDONLY);
+    int dst = open(argv[2], O_WRONLY);
+
+    if (src == -1 || dst == -1) {
+        perror("err");
         exit(1);
     }
 
     char buffer[BUFFER_SIZE];
-    size_t red = 0;
-    FILE* src_ptr = fopen(argv[1], "r");
-    FILE* dst_ptr = fopen(argv[2], "w");
-
-    if (!src_ptr || !dst_ptr) {
-        perror("err: could not open the file.");
-        exit(1);
-    }
+    ssize_t bytes_red = 0;
+    ssize_t bytes_written = 0;
 
     do {
-        red = fread(buffer, sizeof(char), BUFFER_SIZE, src_ptr);
-        fwrite(buffer, sizeof(char), red, dst_ptr);
-    } while (red == BUFFER_SIZE);
+        bytes_red = read(src, buffer, BUFFER_SIZE);
+        bytes_written = write(dst, buffer, bytes_red);
+
+        if (bytes_red != bytes_written || bytes_red == -1 || bytes_written == -1) {
+            perror("err");
+            exit(1);
+        }
+    } while (bytes_red == BUFFER_SIZE);
+
+    close(src);
+    close(dst);
     
     return 0;
 }
