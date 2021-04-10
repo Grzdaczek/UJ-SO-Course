662f956b730ddbbbc8c6ccca89a738079c4fa8e0
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-04-10 15:01:20 +0200

    Zadanie 2 [WIP]

---
+++ zestaw_03/wysylaj.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+int main(int argc, char* argv[])
+{
+    //if(argc != 3) {
+    //    errno = EINVAL;
+    //    perror("Invalid argument count");
+    //    exit(EXIT_FAILURE);
+    //}
+
+    //int sig;
+    //sscanf(argv[2], "%d", &sig);
+    //printf("%d\n", sig);
+
+    int pid = fork();
+
+    execl("obsluga.x", "obsluga.x", "p", "18");
+
+    sleep(1);
+    printf("pid: %d\n", pid);
+    
+    switch(pid) {
+        case -1:
+            perror("Fork error");
+            break;
+        case 0:
+            printf("I'm child!");
+            //execl("./obsluga.x", "./obsluga.x", "p", "18");
+            break;
+        default:
+            if(kill(pid, 0) != -1) {
+                kill(pid, 18);
+            }
+            else {
+                perror("Could not send signal");
+            }
+    }
+
+    return 0;
+}
